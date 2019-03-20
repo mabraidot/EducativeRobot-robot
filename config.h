ec83870cef6e52cf664c3b1f267aa076724645e0
mabraidot <mabraidot@gmail.com>
2019-03-20 16:24:13 -0300

    Pins and constant configurations

---
+++ config.h
@@ -0,0 +1,34 @@
+// PIN
+#define BUZZER                      2
+#define RF_CE                       7
+#define RF_CSN                      8
+#define LIGHT_YELLOW                0
+#define LIGHT_RED                   1
+#define LIGHT_BLUE                  A4
+#define MOTOR_LEFT_ENCODER          3
+#define MOTOR_LEFT_INPUT_1          5   
+#define MOTOR_LEFT_INPUT_2          6
+#define MOTOR_RIGHT_ENCODER         4
+#define MOTOR_RIGHT_INPUT_1         9
+#define MOTOR_RIGHT_INPUT_2         10
+#define SENSOR_LIGHT                A0
+#define SENSOR_SOUND                A1
+#define SENSOR_ULTRASONIC_TRIGGER   A2
+#define SENSOR_ULTRASONIC_ECHO      A3
+
+// RF
+#define RF_CLIENT_ADDRESS           1
+#define RF_SERVER_ADDRESS           2
+#define RF_WAIT_TIMEOUT             7000 // milliseconds
+
+// SLAVE MODES
+#define MODE_MODIFIER_LOOP          1
+#define MODE_SLAVE_FORWARD_ARROW    2
+#define MODE_SLAVE_LEFT_ARROW       2
+#define MODE_SLAVE_RIGHT_ARROW      3
+#define MODE_FUNCTION               4
+
+
+// GENERAL
+#define DEBUG                       true
+#define SILENT                      false
